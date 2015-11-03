451c0fd833a8559af24ccd16da02144cf69cfadb
beugen7171 <beugenx@gmail.com>
2015-11-03 17:31:04 +0000

    Done.

--- spec/first_lab_spec.rb
+++ spec/first_lab_spec.rb
@@ -1,7 +1,7 @@
 require 'spec_helper'
 
-describe 'First Lab:' do
-  it 'you made a change' do
+describe 'First Lab: 1 ' do
+  it 'you made a change, I made a second change, added the number 1' do
     new_file_made = Dir["*"].size > 5
     file_edited = !File.read("./edit-me.txt").empty?
     expect((new_file_made || file_edited)).to be_truthy, "Make sure you have added a new file or edited edit-me.txt"
